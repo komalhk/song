cafe29186b1c899dbaff887f3d09829546eb448f
komal <komalhandibag124@gmail.com>
2024-02-03 05:50:15 +0000

    solution

---
+++ src/main/java/com/example/song/service/SongH2Service.java
@@ -0,0 +1,79 @@
+/*
+ * 
+ * You can use the following import statements
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.jdbc.core.JdbcTemplate;
+ * import org.springframework.stereotype.Service;
+ * import org.springframework.web.server.ResponseStatusException;
+ * import java.util.ArrayList;
+ * 
+ */
+
+// Write your code here
+package com.example.song.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+
+import javax.websocket.server.ServerEndpoint;
+
+import com.example.song.model.Song;
+import com.example.song.model.SongRowMapper;
+import com.example.song.repository.SongRepository;
+
+@Service
+public class SongH2Service implements SongRepository {
+    @Autowired
+    private JdbcTemplate db;
+
+    @Override
+    public ArrayList<Song> getSongs() {
+        return (ArrayList<Song>) db.query("select * from playlist", new SongRowMapper());
+    }
+
+    @Override
+    public Song getSongById(int songId) {
+        try {
+            return db.queryForObject("select * from playlist where songId = ?", new SongRowMapper(), songId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Song addSong(Song song) {
+        db.update("insert into playlist(songName, lyricist, singer, musicdirector) values (?, ?, ?, ?)",
+                song.getSongName(),
+                song.getLyricist(), song.getSinger(), song.getMusicDirector());
+        return db.queryForObject("select * from playlist where songName = ? and lyricist = ?",
+                new SongRowMapper(), song.getSongName(), song.getLyricist());
+    }
+
+    @Override
+    public void deleteSong(int songId) {
+        db.update("delete from playlist where songId = ?", songId);
+    }
+
+    @Override
+    public Song updateSong(int songId, Song song) {
+        if (song.getSongName() != null) {
+            db.update("update playlist set songName = ? where songId = ?", song.getSongName(), songId);
+        }
+        if (song.getLyricist() != null) {
+            db.update("update playlist set lyricist = ? where songId = ?", song.getLyricist(), songId);
+        }
+        if (song.getSinger() != null) {
+            db.update("update playlist set singer = ? where songId = ?", song.getSinger(), songId);
+        }
+        if (song.getMusicDirector() != null) {
+            db.update("update playlist set musicDirector = ? where songId = ?", song.getMusicDirector(), songId);
+        }
+        return getSongById(songId);
+    }
+
+}
